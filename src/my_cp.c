caebc47a9fc5eb3f6986decf5e04ffe1e6ec0947
limitusus <limit.usus@gmail.com>
2013-11-07 01:41:25 +0900

    add -s: sparse option

--- src/my_cp.c
+++ src/my_cp.c
@@ -14,7 +14,7 @@ int is_all_nul(const char* buf, const size_t bufsize) {
   return 1;
 }
 
-void copyfile(const char* from_fname, const char* to_fname) {
+void copyfile(const char* from_fname, const char* to_fname, const int sparse) {
   int in_fd, out_fd;
   ssize_t ret;
   in_fd = open(from_fname, O_RDONLY);
@@ -29,7 +29,7 @@ void copyfile(const char* from_fname, const char* to_fname) {
 #define BUFSIZE 1024
   char buf[BUFSIZE];
   while ((ret = read(in_fd, buf, BUFSIZE)) > 0) {
-    if (is_all_nul(buf, BUFSIZE)) {
+    if (sparse && is_all_nul(buf, BUFSIZE)) {
       lseek(out_fd, BUFSIZE, SEEK_CUR);
     } else {
       ret = write(out_fd, buf, ret);
@@ -47,23 +47,22 @@ void copyfile(const char* from_fname, const char* to_fname) {
 
 int main(int argc, char** argv) {
   int opt;
+  int sparse = 0;
   char* from_fname, *to_fname;
-  /*
-  while ((opt = getopt(argc, argv, "")) != -1) {
+  while ((opt = getopt(argc, argv, "s")) != -1) {
     switch (opt) {
-    case 'a':
-      append = 1;
+    case 's':
+      sparse = 1;
       break;
     default:
-      fprintf(stderr, "Usage: %s [-a] outfile\n", argv[0]);
+      fprintf(stderr, "Usage: %s [-s] fromfile outfile\n", argv[0]);
       exit(EXIT_FAILURE);
     }
   }
-  */
-  if (argc != 3) {
+  if (!((sparse && argc == 4) || argc == 3)) {
     usageErr("Usage: %s FROM TO\n", argv[0]);
   }
-  from_fname = argv[1];
-  to_fname = argv[2];
-  copyfile(from_fname, to_fname);
+  from_fname = argv[1 + sparse];
+  to_fname = argv[2 + sparse];
+  copyfile(from_fname, to_fname, sparse);
 }
